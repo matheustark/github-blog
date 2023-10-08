0e0169de9b1ccc40d704584c50d4234e8de81237
Matheus <matheusglmonteiro@gmail.com>
2023-10-08 16:39:45 -0300

    implementing react-hook-form

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,15 +1,23 @@
+import * as z from 'zod';
 import { SearchContainer, SearchLabelContainer } from "./styles";
+import { useForm } from 'react-hook-form';
 
+const searchFormSchema = z.object({
+    query: z.string(),
+})
+
+type searchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+    const { register, handleSubmit } = useForm<searchFormInputs>()
     return (
-        <SearchContainer>
+        <SearchContainer onSubmit={handleSubmit(data => console.log(data))}>
             <SearchLabelContainer>
             <span>Publicações</span>
             <span> 6 Publicações</span>
             </SearchLabelContainer>
       
-            <input placeholder="Buscar Conteudo" ></input>
+            <input { ...register('query') } type="text" placeholder="Buscar Conteudo" ></input>
         </SearchContainer>
     )
 }
--- src/components/SearchForm/styles.ts
+++ src/components/SearchForm/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const SearchContainer = styled.div`
+export const SearchContainer = styled.form`
     display: flex;
     flex-direction: column;
     width: 100%;
